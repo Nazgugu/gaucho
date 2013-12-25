8ed2865bf99baccdadca8d7984e04acdcaf90524
Nazgugu <prada520farrell@gmail.com>
2013-12-25 00:26:48 -0800

    aaa

--- REMenuExample/AppDelegate.h
+++ REMenuExample/AppDelegate.h
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.h
-//  REMenuExample
-//
-//  Created by Roman Efimov on 2/20/13.
-//  Copyright (c) 2013 Roman Efimov. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import "NavigationViewController.h"
